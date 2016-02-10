f76b5598f7b83d280b0987eefc92e830a2f1f952
Mart Laus <mart.laus@ttu.ee>
2016-02-10 14:18:17 +0200

    Added an extra user, testing Jenkins

--- misc/database/testData.sql
+++ misc/database/testData.sql
@@ -7,6 +7,8 @@ INSERT INTO User (id, email, password, role, created)
 VALUES (2, 'user@user.kz', '$2a$10$R.coZxN.slwkXJ2KxEsa4ufhqk8sGbWha/Me5OmXTYEQgL0XEi6OK', 'USER', NOW());
 INSERT INTO User (id, email, password, role, created)
 VALUES (3, 'asd@asd.kz', '$2a$10$R.coZxN.slwkXJ2KxEsa4ufhqk8sGbWha/Me5OmXTYEQgL0XEi6OK', 'USER', NOW());
+INSERT INTO User (id, email, password, role, created)
+VALUES (3, 'mart@mart.ml', '$2a$10$R.coZxN.slwkXJ2KxEsa4ufhqk8sGbWha/Me5OmXTYEQgL0XEi6OK', 'ADMIN', NOW());
 
 INSERT INTO AuthenticatedUser (id, user_id, token) VALUES (1, 1, 'superUniqueToken');
 
