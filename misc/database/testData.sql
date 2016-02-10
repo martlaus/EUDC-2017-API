2f12dad520808a2c84fdc9a79301d4c6c84c31b9
Mart Laus <mart.laus@ttu.ee>
2016-02-10 11:03:23 +0200

    added gitignore

--- misc/database/testData.sql
+++ misc/database/testData.sql
@@ -5,6 +5,8 @@ INSERT INTO User (id, email, password, role, created)
 VALUES (1, 'admin@admin.kz', '$2a$10$R.coZxN.slwkXJ2KxEsa4ufhqk8sGbWha/Me5OmXTYEQgL0XEi6OK', 'USER', NOW());
 INSERT INTO User (id, email, password, role, created)
 VALUES (2, 'user@user.kz', '$2a$10$R.coZxN.slwkXJ2KxEsa4ufhqk8sGbWha/Me5OmXTYEQgL0XEi6OK', 'USER', NOW());
+INSERT INTO User (id, email, password, role, created)
+VALUES (3, 'asd@asd.kz', '$2a$10$R.coZxN.slwkXJ2KxEsa4ufhqk8sGbWha/Me5OmXTYEQgL0XEi6OK', 'USER', NOW());
 
 INSERT INTO AuthenticatedUser (id, user_id, token) VALUES (1, 1, 'superUniqueToken');
 
