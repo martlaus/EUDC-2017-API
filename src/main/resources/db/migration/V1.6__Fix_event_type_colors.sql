63abc981ec8de422ba592fc0d9ad7442e3257a14
Mart Laus <mart.laus@ttu.ee>
2017-07-29 22:09:23 +0300

    Refactoring

---
+++ src/main/resources/db/migration/V1.6__Fix_event_type_colors.sql
@@ -0,0 +1,11 @@
+USE eudcApi;
+
+UPDATE EventType set color = '#2A2056' where id =1;
+UPDATE EventType set color = '#80A6D7' where id =2;
+UPDATE EventType set eventIcon = 'ion-ios-people', color='#F5B659' where id =3;
+UPDATE EventType set color = '#F6C157' where id =4;
+UPDATE EventType set color = '#43C097' where id =5;
+UPDATE EventType set color = '#49D49D' where id =6;
+UPDATE EventType set color = '#E46173' where id =7;
+UPDATE EventType set color = '#04103E' where id =8;
+
